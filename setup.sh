4324e9ffd21de2ca0d8992196247db05c02c2de2
autopp <autopp.inc@gmail.com>
2015-11-04 16:08:59 +0900

    fix bug and installation oh-my-zsh

--- setup.sh
+++ setup.sh
@@ -8,14 +8,17 @@ function backup() {
   fi
 }
 
-dir=$(dirname $0)
+dir=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
 
 # Setup zsh
 # backup .zshrc
 backup ~/.zshrc
 
 # Install oh-my-zsh
-curl -L https://github.com/robbyrussell/oh-my-zsh/raw/master/tools/install.sh | sh
+if [[ ! -d ~/.oh-my-zsh ]]; then
+  git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
+fi
+cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
 
 # Fix default .zshrc
 sed -i -e 's|source $ZSH/oh-my-zsh.sh|ZSH_THEME="kphoen"\
