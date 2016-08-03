2f6864659a9b817b0e6524853ce56f02f61a64e7
autopp <autopp.inc@gmail.com>
2016-08-03 11:35:54 +0900

    add custom theme

--- setup.sh
+++ setup.sh
@@ -19,9 +19,10 @@ if [[ ! -d ~/.oh-my-zsh ]]; then
   git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 fi
 cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
+cp kphoen-autopp.zsh-theme ~/.oh-my-zsh/themes
 
 # Fix default .zshrc
-sed -i -e 's|source $ZSH/oh-my-zsh.sh|ZSH_THEME="kphoen"\
+sed -i -e 's|source $ZSH/oh-my-zsh.sh|ZSH_THEME="kphoen-autopp"\
 source $ZSH/oh-my-zsh.sh|g' ~/.zshrc
 echo "source ${dir}/.zshrc" >> ~/.zshrc
 echo "# Environment specific configuration" >> ~/.zshrc
