c1a7dd8ced2d19c927c4ed5e3cffd99de20786d3
autopp <autopp.inc@gmail.com>
2016-08-31 18:51:15 +0900

    fix path

--- setup.sh
+++ setup.sh
@@ -19,7 +19,7 @@ if [[ ! -d ~/.oh-my-zsh ]]; then
   git clone git://github.com/robbyrussell/oh-my-zsh.git ~/.oh-my-zsh
 fi
 cp ~/.oh-my-zsh/templates/zshrc.zsh-template ~/.zshrc
-cp kphoen-autopp.zsh-theme ~/.oh-my-zsh/themes
+cp ${dir}/kphoen-autopp.zsh-theme ~/.oh-my-zsh/themes
 
 # Fix default .zshrc
 sed -i -e 's|source $ZSH/oh-my-zsh.sh|ZSH_THEME="kphoen-autopp"\
